799730779ece9478a54b767a17a5c0445f3babce
Oleg Presniakov <ac6776@outlook.com>
2020-03-07 02:08:10 +0300

    PostgreSQL added

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -1,95 +1,85 @@
-SET FOREIGN_KEY_CHECKS = 0;
+-- USERS
+CREATE TABLE public.users
+(
+    id       SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR NOT NULL
+);
 
-DROP TABLE IF EXISTS users;
+-- ROLES
+CREATE TABLE public.roles
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL
+);
 
-CREATE TABLE users (
-                       id                    INT(11) NOT NULL AUTO_INCREMENT,
-                       username              VARCHAR(50) NOT NULL,
-                       password              CHAR(80) NOT NULL,
-                       PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+-- NOTES
+CREATE TABLE public.notes
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-DROP TABLE IF EXISTS roles;
+-- USERS_NOTES
+CREATE TABLE public.users_notes
+(
+    user_id INTEGER NOT NULL,
+    note_id INTEGER NOT NULL,
 
-CREATE TABLE roles (
-                       id                    INT(11) NOT NULL AUTO_INCREMENT,
-                       name                  VARCHAR(50) DEFAULT NULL,
-                       PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+    PRIMARY KEY (user_id,note_id),
 
-DROP TABLE IF EXISTS notes;
+    FOREIGN KEY (user_id) REFERENCES public.users (id)
+        ON DELETE CASCADE ON UPDATE NO ACTION,
 
-CREATE TABLE notes (
-                       id                   INT(11) NOT NULL AUTO_INCREMENT,
-                       title                VARCHAR(50) NOT NULL,
-                       body                 TEXT NOT NULL,
-                       creation_date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+    FOREIGN KEY (note_id) REFERENCES public.notes (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
 
+-- USERS_ROLES
+CREATE TABLE public.users_roles
+(
+    user_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
 
-DROP TABLE IF EXISTS users_notes;
+    PRIMARY KEY (user_id,role_id),
 
-CREATE TABLE users_notes (
-                             user_id               INT(11) NOT NULL,
-                             note_id               INT(11) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES public.users (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
 
-                             PRIMARY KEY (user_id,note_id),
+    FOREIGN KEY (role_id) REFERENCES public.roles (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
 
-                             CONSTRAINT FK_USER_NOTE_ID FOREIGN KEY (user_id)
-                                 REFERENCES users (id)
-                                 ON DELETE NO ACTION ON UPDATE NO ACTION,
-    #                                  ON DELETE CASCADE ON UPDATE NO ACTION,
-
-                             CONSTRAINT FK_NOTE_ID FOREIGN KEY (note_id)
-                                 REFERENCES notes (id)
-                                 ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS users_roles;
-
-CREATE TABLE users_roles (
-                             user_id               INT(11) NOT NULL,
-                             role_id               INT(11) NOT NULL,
-
-                             PRIMARY KEY (user_id,role_id),
-
-                             CONSTRAINT FK_USER_ID_01 FOREIGN KEY (user_id)
-                                 REFERENCES users (id)
-                                 ON DELETE NO ACTION ON UPDATE NO ACTION,
-
-                             CONSTRAINT FK_ROLE_ID FOREIGN KEY (role_id)
-                                 REFERENCES roles (id)
-                                 ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-INSERT INTO roles (name)
+-- INSERTS
+INSERT INTO public.users (username, password)
 VALUES
-('ROLE_USER'), ('ROLE_ADMIN');
-
-INSERT INTO users (username,password)
+('Bob', '$2a$10$P9E9Yp1vgAtCrj4viVaSk.PTR7kleLww1N6e1T0OK8rH0Tj6lN7wm'),
+('Alex', '$2a$10$P9E9Yp1vgAtCrj4viVaSk.PTR7kleLww1N6e1T0OK8rH0Tj6lN7wm')
+;
+INSERT INTO public.roles (name)
 VALUES
-('Alex','$2a$10$P9E9Yp1vgAtCrj4viVaSk.PTR7kleLww1N6e1T0OK8rH0Tj6lN7wm'),
-('Bob', '$2a$10$P9E9Yp1vgAtCrj4viVaSk.PTR7kleLww1N6e1T0OK8rH0Tj6lN7wm');
-
-INSERT INTO notes (title, body)
+('ROLE_ADMIN'),
+('ROLE_USER')
+;
+INSERT INTO public.notes (title, body)
 VALUES
 ('Title 1', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'),
 ('Title 2', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'),
 ('Title 3', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'),
-('Title 4', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...');
-
-INSERT INTO users_roles (user_id, role_id)
-VALUES
-(1, 1),
-(1, 2),
-(2, 1);
-
-INSERT INTO users_notes (user_id, note_id)
+('Title 4', 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...')
+;
+INSERT INTO public.users_notes (user_id, note_id)
 VALUES
 (1, 1),
 (1, 2),
 (2, 3),
-(2, 4);
+(2, 4)
+;
+INSERT INTO public.users_roles (user_id, role_id)
+VALUES
+(1, 1),
+(1, 2),
+(2, 2)
+;
